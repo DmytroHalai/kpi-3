59635b02b7f7ce23762ea3f8f204f4df6876a02e
Solomka Andrii <andriissolomka@gmail.com>
2025-04-16 23:13:01 +0300

    Add draw-diagonal.go script for diagonal movement functionality

--- scripts/draw-diagonal.go
+++ scripts/draw-diagonal.go
@@ -48,6 +48,7 @@ func moveAlongSegment(x1, y1, x2, y2 float64, steps int, delay time.Duration) er
 
 func main() {
 	commands := []string{
+		"reset",
 		"green",
 		"bgrect 0.25 0.25 0.75 0.75",
 		"figure 0.5 0.5",
@@ -61,24 +62,20 @@ func main() {
 		}
 	}
 
-	points := [][2]float64{
-		{0.25, 0.25}, // A
-		{0.75, 0.25}, // B
-		{0.75, 0.75}, // C
-		{0.25, 0.75}, // D
-	}
+	start := [2]float64{0.25, 0.25} 
+	end := [2]float64{0.75, 0.75}   
 
-	stepsPerEdge := 50
+	steps := 100
 	delay := 50 * time.Millisecond
 
 	for {
-		for i := 0; i < len(points); i++ {
-			start := points[i]
-			end := points[(i+1)%len(points)]
-			if err := moveAlongSegment(start[0], start[1], end[0], end[1], stepsPerEdge, delay); err != nil {
-				fmt.Printf("Error moving along segment: %v\n", err)
-				return
-			}
+		if err := moveAlongSegment(start[0], start[1], end[0], end[1], steps, delay); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
+		}
+		if err := moveAlongSegment(end[0], end[1], start[0], start[1], steps, delay); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			return
 		}
 	}
 }
